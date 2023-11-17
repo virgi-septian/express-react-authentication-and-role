644fc25986a97906d704cc03eee139c023022f3e
virgi-septian <virgiseptian77@gmail.com>
2023-11-17 16:27:26 +0700

    setup file controllers

--- authentication/backend/controllers/Products.js
+++ authentication/backend/controllers/Products.js
@@ -1,2 +1,21 @@
 import Product from'../models/ProductModel.js';
 
+export const getProducts = (req, res) => {
+    
+}
+
+export const getProductById = (req, res) => {
+
+}
+
+export const createdProduct = (req, res) => {
+
+}
+
+export const updatedProduct = (req, res) => {
+
+}
+
+export const deleteProduct = (req, res) => {
+
+}
---
+++ authentication/backend/controllers/Users.js
@@ -0,0 +1,21 @@
+import User from'../models/UserModel.js';
+
+export const getUsers = (req, res) => {
+    
+}
+
+export const getUserById = (req, res) => {
+
+}
+
+export const createdUser = (req, res) => {
+
+}
+
+export const updatedUser = (req, res) => {
+
+}
+
+export const deleteUser = (req, res) => {
+
+}
