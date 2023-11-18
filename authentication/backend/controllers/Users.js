b693dd9eebb694ac6cb99bef8798ced43d1a5783
virgi-septian <virgiseptian77@gmail.com>
2023-11-18 23:46:48 +0700

    tryin to use rest client extention

--- authentication/backend/controllers/Users.js
+++ authentication/backend/controllers/Users.js
@@ -1,15 +1,49 @@
 import User from'../models/UserModel.js';
+import argon2 from 'argon2';
 
-export const getUsers = (req, res) => {
-    
+export const getUsers = async(req, res) => {
+    try {
+        const response = await User.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
-export const getUserById = (req, res) => {
+export const getUserById = async(req, res) => {
+    try {
+        const response = await User.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        });
 
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }
 
-export const createdUser = (req, res) => {
+export const createdUser = async(req, res) => {
+    console.log(req.body);
+    const {name, email, password, confPassword, role} = req.body;
+    console.log(password);
 
+    if(password !== confPassword) return res.status(400).json({msg: `password dan confirm password tidak cocok`});
+
+    const hashPassword = await argon2.hash(password);
+
+    try {
+        await User.create({
+            name: name,
+            email: email,
+            password: password,
+            role: role,
+        })
+        res.status(201).json({mgs: `register berhasil`});
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
 }
 
 export const updatedUser = (req, res) => {
--- authentication/backend/routes/UserRoute.js
+++ authentication/backend/routes/UserRoute.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
-router.post('/users', createdUser);
+router.post('/users/create', createdUser);
 router.patch('/users/:id', updatedUser);
 router.delete('/users/:id', deleteUser);
 
