e3238e04b931e75e55807a0382f5b4eca4242aef
virgi-septian <virgiseptian77@gmail.com>
2023-11-26 14:22:27 +0700

    Login asyncThunk

--- authentication/frontend/src/features/authSlice.jsx
+++ authentication/frontend/src/features/authSlice.jsx
@@ -9,10 +9,44 @@ const initialState = {
   message: ""
 }
 
-export const authSlice = createAsyncThunk({
+export const LoginUser = createAsyncThunk("user/loginUser", async (user, thunkApi )=> {
+  try {
+    const response = await axios.post('http://localhost:5000/login', {
+      email: user.email,
+      password: user.password,
+    })
+
+    return response.data;
+  } catch (error) {
+    if(error.response){
+      const message = error.response.data.msg;
+      return thunkApi.rejectWithValue(message);
+    }
+  }
+});
+
+export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers:{
     reset: (state) => initialState, 
+  },
+  extraReducers: (builder) => {
+    builder.addCase(LoginUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(LoginUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.user = action.payload;
+    });
+    builder.addCase(LoginUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+    })
   }
 });
+
+export const {reset} = authSlice.action;
+export default authSlice.reducers;
